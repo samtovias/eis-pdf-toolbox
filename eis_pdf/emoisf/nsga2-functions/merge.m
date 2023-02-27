8468b290d240b5fba01725f0eae1af02937ee66d
samtovias <57341323+samtovias@users.noreply.github.com>
2023-02-27 12:01:12 -0600

    Delete eis_pdf/emoisf directory

--- eis_pdf/emoisf/nsga2-functions/merge.m
+++
@@ -1,6 +0,0 @@
-function pop3 = merge(pop1, pop2, popsize)
-
-pop3 = [pop1; pop2];
-
-
-
--- eis_pdf/emoisf/nsga2-sbe-functions/decode_sbe.m
+++
@@ -1,9 +0,0 @@
-
-% Decode for SBE 
-% not decoding at all, just inserts the binary string in parents pop matrix
-
-function pop = decode_sbe(pop, pop_strings, nobj, ncon, nreal)
-
-    pop(:,nobj+ncon+nreal+1:end-3) = pop_strings;  
-
-end
